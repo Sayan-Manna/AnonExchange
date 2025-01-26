acdbc167b983ba87712c2f122f92e3c5a2789495
Sayan Manna <rexsayan028@gmail.com>
2025-01-26 16:05:01 +0530

    resend edit

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+      from: "rexsayan028@gmail.com",
       to: email,
       subject: "AnonExchange Verification Code :: ☠️ ",
       react: VerificationEmail({ username, otp: verifyCode }),
